6b94e1e78e1c80266310f1889f9438be609e4531
YouYou94 <ybh4115@gmail.com>
2022-11-01 20:35:02 +0900

    TopBar. export

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Routes } from 'react-router-dom';
 import './App.css';
+import TopBar from './components/TopBar/TopBar.jsx';
 
 function App() {
   return (
     <div className="App">
+      <TopBar />
       <Routes></Routes>
     </div>
   );
