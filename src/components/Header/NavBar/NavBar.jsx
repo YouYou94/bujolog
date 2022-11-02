d34be84e7f11270601dc9c5b3ec2586b86cfb2f4
YouYou94 <ybh4115@gmail.com>
2022-11-02 13:30:53 +0900

    Header. NavBar. 메뉴 아이템 변경

--- src/components/Header/NavBar/NavBar.jsx
+++ src/components/Header/NavBar/NavBar.jsx
@@ -1,12 +1,10 @@
 import * as Styled from '../styled.jsx';
-import { Home, Bujotalk, Mybujo, Logout } from './NavItem.jsx';
+import { Mybujo, Logout } from './NavItem.jsx';
 
 const NavBar = () => {
   return (
     <Styled.HeaderNavBar>
-      <Home />
       <Mybujo />
-      <Bujotalk />
       <Logout />
     </Styled.HeaderNavBar>
   );
