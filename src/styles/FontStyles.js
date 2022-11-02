371753e9e6a65cca90defa2fd3538ade76333001
YouYou94 <ybh4115@gmail.com>
2022-11-02 17:04:59 +0900

    FontStyles. 폰트 추가 3종

--- src/styles/FontStyles.js
+++ src/styles/FontStyles.js
@@ -1,11 +1,29 @@
 import { createGlobalStyle } from 'styled-components';
 import Nabla from './fonts/Nabla-Regular.ttf';
+import BlackHanSans from './fonts/BlackHanSans-Regular.ttf';
+import YeonSung from './fonts/YeonSung-Regular.ttf';
+import GowunDodum from './fonts/GowunDodum-Regular.ttf';
 
 const FontStyles = createGlobalStyle`
 
 @font-face {
     font-family: 'Nabla';
     src: url(${Nabla}) format('truetype');
+}
+
+@font-face {
+    font-family: 'BlackHanSans';
+    src: url(${BlackHanSans}) format('truetype');
+}
+
+@font-face {
+    font-family: 'YeonSung';
+    src: url(${YeonSung}) format('truetype');
+}
+
+@font-face {
+    font-family: 'GowunDodum';
+    src: url(${GowunDodum}) format('truetype');
 }
   
 `;
