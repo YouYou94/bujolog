de8de7ef83c97c48afef5520c15673567d130af7
YouYou94 <ybh4115@gmail.com>
2022-11-03 13:48:49 +0900

    Auth. App.js useEffect를 이용하여 로그인 여부 확인

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import { BrowserRouter } from 'react-router-dom';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Router from './Router/Router.jsx';
 import './App.css';
+import { authService } from './Firebase.js';
+import { onAuthStateChanged } from 'firebase/auth';
 
 function App() {
-  const [isLogin, setIsLogin] = useState(true);
+  const [isLogin, setIsLogin] = useState(false);
+
+  useEffect(() => {
+    onAuthStateChanged(authService, user => {
+      if (user) {
+        setIsLogin(true);
+      } else {
+        setIsLogin(false);
+      }
+    });
+  }, []);
 
   return (
     <div className="App">
