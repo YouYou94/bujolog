e541d3295f6e5da5eaec3c571d550740342a5216
YouYou94 <ybh4115@gmail.com>
2022-11-03 14:30:54 +0900

    Auth. NavItem. 로그아웃 컴포넌트 로그아웃 기능 구현

--- src/components/Header/NavBar/NavItem.jsx
+++ src/components/Header/NavBar/NavItem.jsx
@@ -1,4 +1,6 @@
 import { useNavigate } from 'react-router-dom';
+import { authService } from '../../../Firebase.js';
+import Swal from 'sweetalert2';
 import * as Styled from '../styled.jsx';
 
 /* On Login */
@@ -21,12 +23,27 @@ export const Bujotalk = () => {
 
 export const Logout = () => {
   const navigate = useNavigate();
-  return (
-    <Styled.HeaderNavItem
-      onClick={() => {
+
+  const onHandlerSignOut = () => {
+    Swal.fire({
+      title: '로그아웃하시겠습니까?',
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: '확인',
+      cancelButtonText: '취소',
+    }).then(async result => {
+      if (result.isConfirmed) {
+        Swal.fire('로그아웃!', '로그아웃하셨습니다. 감사합니다.', 'success');
+        await authService.signOut();
         navigate('/bujolog');
-      }}
-    >
+      }
+    });
+  };
+
+  return (
+    <Styled.HeaderNavItem onClick={onHandlerSignOut}>
       로그아웃
     </Styled.HeaderNavItem>
   );
