76be7d6e6623de9ea68aa57dd5e596c408352544
YouYou94 <ybh4115@gmail.com>
2022-11-03 13:47:33 +0900

    Auth. 구글 및 깃허브 로그인 연동 완료

--- src/components/Auth/GithubLogin.jsx
+++ src/components/Auth/GithubLogin.jsx
@@ -1,10 +1,27 @@
 import * as Styled from './styled.jsx';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useNavigate } from 'react-router-dom';
+import { authService } from '../../Firebase.js';
+import { GithubAuthProvider, signInWithPopup } from 'firebase/auth';
 
 const GithubLogin = () => {
+  const navigate = useNavigate();
+  const onSocialLoginWithGithub = async () => {
+    const provider = new GithubAuthProvider();
+
+    await signInWithPopup(authService, provider)
+      .then(result => console.log(result))
+      .catch(error => console.log(error));
+
+    navigate('/bujolog');
+  };
+
   return (
-    <Styled.AuthSocialLoginButton color="black">
+    <Styled.AuthSocialLoginButton
+      color="black"
+      onClick={onSocialLoginWithGithub}
+    >
       <FontAwesomeIcon icon={faGithub} /> Github
     </Styled.AuthSocialLoginButton>
   );
--- src/components/Auth/GoogleLogin.jsx
+++ src/components/Auth/GoogleLogin.jsx
@@ -1,10 +1,25 @@
 import * as Styled from './styled.jsx';
 import { faGoogle } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useNavigate } from 'react-router-dom';
+import { authService } from '../../Firebase.js';
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 
 const GoogleLogin = () => {
+  const navigate = useNavigate();
+  const onSocialLoginWithGoogle = async () => {
+    const provider = new GoogleAuthProvider();
+    await signInWithPopup(authService, provider)
+      .then(result => console.log(result))
+      .catch(error => console.log(error));
+
+    navigate('/bujolog');
+  };
   return (
-    <Styled.AuthSocialLoginButton color="#4285F4">
+    <Styled.AuthSocialLoginButton
+      color="#4285F4"
+      onClick={onSocialLoginWithGoogle}
+    >
       <FontAwesomeIcon icon={faGoogle} /> Google
     </Styled.AuthSocialLoginButton>
   );
