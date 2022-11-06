68c0ada217d2eaf262143dd3b0b02d283f6acae1
YouYou94 <ybh4115@gmail.com>
2022-11-06 22:31:21 +0900

    Nav. 사용자 메뉴 추가 (나중에 로그인 여부 확인하기!)

--- src/components/TopBar/NavContents/Menu.jsx
+++ src/components/TopBar/NavContents/Menu.jsx
@@ -1,7 +1,13 @@
+import MyBujo from './MenuItem/MyBujo.jsx';
 import * as Styled from './NavContentsStyled.jsx';
 
+// 로그인 여부 확인하기!
 const Menu = () => {
-  return <Styled.MenuContainer></Styled.MenuContainer>;
+  return (
+    <Styled.MenuContainer>
+      <MyBujo />
+    </Styled.MenuContainer>
+  );
 };
 
 export default Menu;
---
+++ src/components/TopBar/NavContents/MenuItem/MyBujo.jsx
@@ -0,0 +1,7 @@
+import * as Styled from '../NavContentsStyled.jsx';
+
+const MyBujo = () => {
+  return <Styled.MenuItem>나의 불렛저널</Styled.MenuItem>;
+};
+
+export default MyBujo;
