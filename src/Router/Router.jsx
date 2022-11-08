a1b2e758ecc1f7d8cb1fcb02050ed48796f8ac05
YouYou94 <ybh4115@gmail.com>
2022-11-08 13:53:38 +0900

    feat. 라우터에서 경로 확인 후 NotFound 경로일 경우 NotFound 페이지 display

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,12 +1,22 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
+
+// Components
 import TopBar from '../components/TopBar/TopBar.jsx';
+
+// Page
 import AuthPage from '../pages/AuthPage.jsx';
+import NotFound from '../pages/NotFound.jsx';
+
+const EFFECTIVEPATH = ['/bujolog', '/bujolog/auth/login', '/bujolog/mine'];
 
 const Router = () => {
+  const nowPath = useLocation().pathname;
   return (
     <>
-      <TopBar />
+      {EFFECTIVEPATH.find(path => path === nowPath) ? <TopBar /> : <></>}
       <Routes>
+        {/* NotFound */}
+        <Route path="/*" element={<NotFound />} />
         {/* Auth */}
         <Route path="/bujolog/auth/login" element={<AuthPage />} />
       </Routes>
