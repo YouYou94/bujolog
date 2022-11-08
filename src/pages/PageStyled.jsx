ac422792ffb4e0edd234d68908ec6cbfe5863a4b
YouYou94 <ybh4115@gmail.com>
2022-11-08 13:51:55 +0900

    NotFound. 페이지 styled-components 구현

--- src/pages/PageStyled.jsx
+++ src/pages/PageStyled.jsx
@@ -25,3 +25,60 @@ export const AuthContainer = styled.div`
 
   margin-bottom: 10rem;
 `;
+
+/* Not Found Page */
+export const NotFoundContainer = styled.div`
+  flex: 1;
+
+  display: flex;
+  flex-direction: column;
+`;
+
+export const NotFoundTitle = styled.div`
+  margin-bottom: 2rem;
+
+  font-family: 'CormorantGaramond';
+  font-size: 4rem;
+
+  cursor: pointer;
+`;
+
+export const NotFoundSubTitle = styled.div`
+  margin: 0 0 0.5rem 1rem;
+
+  font-family: 'GowunDodum';
+  font-size: 1.5rem;
+`;
+
+export const NotFoundExplanation = styled.div`
+  margin: 0 0 0.3rem 1rem;
+
+  font-family: 'GowunDodum';
+  font-size: 1.1rem;
+`;
+
+export const NotFoundWrapper = styled.div`
+  margin: 2rem 0;
+
+  text-align: center;
+`;
+
+export const NotFoundButton = styled.button`
+  width: 25rem;
+  height: 2rem;
+
+  border: 1px solid gray;
+  border-radius: 2rem;
+
+  font-family: 'GowunDodum';
+  font-size: 1rem;
+
+  color: black;
+  background-color: white;
+
+  cursor: pointer;
+
+  &:hover {
+    color: gray;
+  }
+`;
