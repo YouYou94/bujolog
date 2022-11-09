00be6b0ddf40b2bf428f6b72c2f2d2870dfb60a5
YouYou94 <ybh4115@gmail.com>
2022-11-09 18:01:52 +0900

    Redux. 로그인 여부 모듈 완성

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
-import jourReducer from './Modules/Journal';
-import topBarMenuReducer from './Modules/TopBarMenuDisplay';
+import jourReducer from './Modules/Journal.jsx';
+import topBarMenuReducer from './Modules/TopBarMenuDisplay.jsx';
+import isLoginReducer from './Modules/IsLogin.jsx';
 
 const AppReducer = combineReducers({
+  isLoginReducer,
   jourReducer,
   topBarMenuReducer,
 });
