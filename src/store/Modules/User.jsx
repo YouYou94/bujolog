7e0ef32f8c0edb1ef35befe7acbc532971ee6937
YouYou94 <ybh4115@gmail.com>
2022-11-09 18:48:37 +0900

    Redux. 사용자 모듈 구현

---
+++ src/store/Modules/User.jsx
@@ -0,0 +1,44 @@
+/* Initial Value */
+const initialState = {
+  name: null,
+  userId: null,
+};
+
+/* Action Type */
+const USERSETTING = 'USERSETTING';
+const USERINITIAL = 'USERINITIAL';
+
+/* Action */
+export const userSetting = ({ displayName, uId }) => {
+  return {
+    type: USERSETTING,
+    name: displayName,
+    userId: uId,
+  };
+};
+
+export const userInitial = () => {
+  return { type: USERINITIAL };
+};
+
+/* Reducer */
+const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case USERSETTING:
+      return {
+        ...state,
+        name: action.name,
+        userId: action.uId,
+      };
+    case USERINITIAL:
+      return {
+        ...state,
+        name: null,
+        userId: null,
+      };
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
--- src/store/store.js
+++ src/store/store.js
@@ -3,9 +3,11 @@ import { combineReducers } from 'redux';
 import jourReducer from './Modules/Journal.jsx';
 import topBarMenuReducer from './Modules/TopBarMenuDisplay.jsx';
 import isLoginReducer from './Modules/IsLogin.jsx';
+import userReducer from './Modules/User.jsx';
 
 const AppReducer = combineReducers({
   isLoginReducer,
+  userReducer,
   jourReducer,
   topBarMenuReducer,
 });
