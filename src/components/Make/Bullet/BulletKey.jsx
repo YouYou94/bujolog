12d1433a9192817486488cde7d3ea758601c9205
YouYou94 <ybh4115@gmail.com>
2022-11-11 20:29:40 +0900

    수정. Bullet 컴포넌트 초기화

--- src/components/Make/Bullet/BulletKey.jsx
+++
@@ -1,7 +0,0 @@
-import * as Styled from './BulletStyled.jsx';
-
-const BulletKey = () => {
-  return <Styled.BulletKeyWrap></Styled.BulletKeyWrap>;
-};
-
-export default BulletKey;
--- src/components/Make/Container/BulletContainer.jsx
+++ src/components/Make/Container/BulletContainer.jsx
@@ -1,15 +1,11 @@
-import BulletKey from '../Bullet/BulletKey.jsx';
-import BulletProgress from '../Bullet/BulletProgress.jsx';
-import BulletTitle from '../Bullet/BulletTitle.jsx';
 import * as Styled from './ContainerStyled.jsx';
 
 const BulletContainer = () => {
   return (
-    <Styled.JournalMakingContainer>
-      <BulletTitle />
-      <BulletKey />
-      <BulletProgress />
-    </Styled.JournalMakingContainer>
+    <>
+      <Styled.JournalMakingContainer></Styled.JournalMakingContainer>
+      <Styled.JournalMakingContainer></Styled.JournalMakingContainer>
+    </>
   );
 };
 
