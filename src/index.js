697e19996fedfc100f2376d5c0210a1a60c13c19
YouYou94 <ybh4115@gmail.com>
2022-11-14 16:01:26 +0900

    Fix. styled-components 폰트 깜빡임 문제 해결

--- src/index.js
+++ src/index.js
@@ -14,7 +14,6 @@ import './Reset.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <FontStyles />
     <App />
   </React.StrictMode>
 );
