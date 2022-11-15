763d3a64e388deecb20026379f22424e7450140d
YouYou94 <ybh4115@gmail.com>
2022-11-15 18:59:15 +0900

    변경. 사용자 메뉴

--- src/components/SideMenu/SideMenuUser.jsx
+++ src/components/SideMenu/SideMenuUser.jsx
@@ -1,6 +1,7 @@
 import { useRef } from 'react';
 import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { close } from '../../store/modules/SideMenuModule.jsx';
 
 import * as Styled from './SideMenuStyled.jsx';
@@ -9,6 +10,7 @@ const SideMenuUser = () => {
   const [isDisplay, setIsDisplay] = useState(false);
   const ref = useRef();
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleClickOutSide = e => {
     if (isDisplay && !ref.current.contains(e.target)) {
@@ -23,20 +25,36 @@ const SideMenuUser = () => {
     };
   });
 
+  const goMyPage = () => {
+    dispatch(close());
+    navigate('/bujolog/mine');
+  };
+
+  const goBulletSetting = () => {
+    dispatch(close());
+    navigate('/bujolog/mine/bullet');
+  };
+
+  const writeJournal = () => {
+    dispatch(close());
+    navigate('/bujolog/mine/journal');
+  };
+
   return (
     <Styled.AccodianContainer ref={ref}>
       <Styled.AccodianHeader onClick={() => setIsDisplay(!isDisplay)}>
         <Styled.AccodianTitle>유저 메뉴</Styled.AccodianTitle>
       </Styled.AccodianHeader>
       <Styled.AccodianArticle isDisplay={isDisplay}>
-        <Styled.AccodianItem onClick={() => dispatch(close())}>
-          메뉴 01
+        <Styled.AccodianItem onClick={goMyPage}>
+          마이 페이지
         </Styled.AccodianItem>
-        <Styled.AccodianItem onClick={() => dispatch(close())}>
-          메뉴 02
+        <Styled.AccodianItem onClick={goBulletSetting}>
+          불렛 설정하기
         </Styled.AccodianItem>
-        <Styled.AccodianItem onClick={() => dispatch(close())}>
-          메뉴 03
+        <Styled.AccodianItem>만다라트 (준비중)</Styled.AccodianItem>
+        <Styled.AccodianItem onClick={writeJournal}>
+          저널 작성하기
         </Styled.AccodianItem>
       </Styled.AccodianArticle>
     </Styled.AccodianContainer>
