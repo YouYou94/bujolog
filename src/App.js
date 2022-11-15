d8a85cb149163baba40f77a233a884ab00ab9009
YouYou94 <ybh4115@gmail.com>
2022-11-15 16:01:47 +0900

    Add. 로그인 여부 확인 후 사용자 정보 받기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import { BrowserRouter } from 'react-router-dom';
-import AppRouter from './router/AppRouter';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { onAuthStateChanged } from 'firebase/auth';
+import { authService } from './Firebase.js';
+import { defaultUser, setUser } from './store/modules/UserModule.jsx';
+import { setTrueLogin, setFalseLogin } from './store/modules/IsLoginModule.jsx';
+import AppRouter from './router/AppRouter.jsx';
 import './App.css';
 
 function App() {
+  const dispatch = useDispatch();
+  const is = useSelector(state => state.isLogin);
+  console.log(is);
+
+  useEffect(() => {
+    onAuthStateChanged(authService, user => {
+      if (user) {
+        dispatch(setUser({ name: user.displayName, userId: user.uid }));
+        dispatch(setTrueLogin());
+      } else {
+        dispatch(defaultUser());
+        dispatch(setFalseLogin());
+      }
+    });
+  }, []);
+
   return (
     <div className="App">
       <BrowserRouter>
