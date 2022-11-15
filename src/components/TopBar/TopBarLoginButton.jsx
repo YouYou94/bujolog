07be4ff5665b160d2ae38ac92a1a9c4825c15dda
YouYou94 <ybh4115@gmail.com>
2022-11-15 11:31:10 +0900

    Add. TopBar Login Button 클릭 시 로그인 페이지 이동

--- src/components/TopBar/TopBarLoginButton.jsx
+++ src/components/TopBar/TopBarLoginButton.jsx
@@ -1,7 +1,12 @@
+import { useNavigate } from 'react-router-dom';
 import * as Styled from './TopBarStyled.jsx';
 
 const TopBarLoginButton = () => {
-  return <Styled.LoginButton>로그인</Styled.LoginButton>;
+  const navigate = useNavigate();
+
+  const goAuth = () => navigate('/bujolog/auth/login');
+
+  return <Styled.LoginButton onClick={goAuth}>로그인</Styled.LoginButton>;
 };
 
 export default TopBarLoginButton;
