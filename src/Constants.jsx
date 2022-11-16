a8097bae9bdeea9e5c65ff54ac9b5c9d8bd98921
YouYou94 <ybh4115@gmail.com>
2022-11-16 19:13:16 +0900

    수정. 불렛 아이콘 리스트 전역 상수로 관리

---
+++ src/Constants.jsx
@@ -0,0 +1,35 @@
+import square from './img/BulletIcon/square.png';
+import fillsquare from './img/BulletIcon/fillsquare.png';
+import triangle from './img/BulletIcon/triangle.png';
+import filltriangle from './img/BulletIcon/filltriangle.png';
+import circle from './img/BulletIcon/circle.png';
+import fillcircle from './img/BulletIcon/fillcircle.png';
+import dot from './img/BulletIcon/dot.png';
+import x from './img/BulletIcon/x.png';
+import up from './img/BulletIcon/up.png';
+import down from './img/BulletIcon/down.png';
+import next from './img/BulletIcon/next.png';
+import prev from './img/BulletIcon/prev.png';
+import cake from './img/BulletIcon/cake.png';
+import star from './img/BulletIcon/star.png';
+import heart from './img/BulletIcon/heart.png';
+import music from './img/BulletIcon/music.png';
+
+export const ICONLIST = [
+  square,
+  fillsquare,
+  triangle,
+  filltriangle,
+  circle,
+  fillcircle,
+  dot,
+  x,
+  up,
+  down,
+  next,
+  prev,
+  cake,
+  star,
+  heart,
+  music,
+];
