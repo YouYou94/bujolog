79aca23d0acce6dced11d8d2082922766c99aab6
YouYou94 <ybh4115@gmail.com>
2022-11-16 20:28:40 +0900

    Add. 불렛 리스트 및 세팅 추가

--- src/components/Bullet/BulletSelect.jsx
+++ src/components/Bullet/BulletSelect.jsx
@@ -1,7 +1,46 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { addBullet, delBullet } from '../../store/modules/BulletModuls.jsx';
+import { ICONLIST } from '../../Constants.jsx';
 import * as Styled from './BulletStyled.jsx';
 
 const BulletSelect = () => {
-  return <Styled.SelectWrapper></Styled.SelectWrapper>;
+  const common = useSelector(state => state.bullet.commonState);
+  //   const user = useSelector(state => state.bullet.userState);
+  const dispatch = useDispatch();
+  //   console.log('common', common);
+  //   console.log('user', user);
+  const onHandlerAddBullet = () => {
+    dispatch(addBullet({ key: '', name: '', value: '' }));
+  };
+
+  const onHandlerDelBullet = event => {
+    dispatch(delBullet(Number(event.target.parentElement.id)));
+  };
+
+  const list = common.bullet.map((item, index) => {
+    return (
+      <Styled.BulletList id={index} key={index}>
+        <Styled.Icon icon={item.key} />
+        <Styled.Name>{item.name}</Styled.Name>
+        <Styled.Description>{item.description}</Styled.Description>
+        <Styled.DeleteButton onClick={onHandlerDelBullet}>
+          삭제
+        </Styled.DeleteButton>
+      </Styled.BulletList>
+    );
+  });
+
+  return (
+    <Styled.SelectWrapper>
+      <Styled.SelectTitle>your key</Styled.SelectTitle>
+      <Styled.Wrapper>
+        {list}
+        <Styled.AddWrapper>
+          <Styled.Add onClick={onHandlerAddBullet}>+</Styled.Add>
+        </Styled.AddWrapper>
+      </Styled.Wrapper>
+    </Styled.SelectWrapper>
+  );
 };
 
 export default BulletSelect;
--- src/components/Bullet/BulletStyled.jsx
+++ src/components/Bullet/BulletStyled.jsx
@@ -5,14 +5,14 @@ import styled from 'styled-components';
 */
 /* Title */
 export const Title = styled.h1`
-  height: 15%;
+  height: 10rem;
 
   padding: 3rem 2rem 0 2rem;
 
   font-family: 'CormorantGaramond';
   font-size: 3.5rem;
 
-  border-bottom: 1px solid rgb(220, 220, 220);
+  border-bottom: 3px solid rgb(220, 220, 220);
 
   text-align: center;
 
@@ -48,7 +48,7 @@ export const Option = styled.div`
   cursor: pointer;
 
   &:hover {
-    background-color: rgb(255, 211, 117, 0.2);
+    background-color: rgb(255, 211, 117, 0.1);
   }
 `;
 
@@ -67,13 +67,118 @@ export const OptionItem = styled.div`
 /* Select */
 export const SelectWrapper = styled.div`
   flex: 1;
+  height: 100vh;
 
+  display: flex-block;
+  flex-direction: column;
+
+  border-bottom: 3px solid rgb(220, 220, 220);
+
+  overflow: hidden;
+`;
+
+export const SelectTitle = styled.h1`
+  height: 10rem;
+
+  font-family: 'CormorantGaramond';
+  font-size: 3.5rem;
+
+  padding: 3rem 2rem 0 2rem;
+  border-bottom: 3px solid rgb(220, 220, 220);
+
+  text-align: center;
+
+  user-select: none;
+`;
+
+export const Wrapper = styled.div`
+  height: 73vh;
+
+  overflow-y: auto;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+export const BulletList = styled.div`
+  height: 5rem;
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  margin: 0.5rem 0;
+  padding: 1rem 1rem;
+
+  border-top: 1px solid rgb(220, 220, 220);
   border-bottom: 1px solid rgb(220, 220, 220);
+`;
+
+export const Icon = styled.div`
+  width: 2rem;
+  height: 2rem;
+
+  background-image: url(${prop => prop.icon});
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
+
+export const Name = styled.span`
+  flex: 1;
+
+  text-align: center;
+`;
+
+export const Description = styled.span`
+  flex: 1;
+
+  text-align: center;
+`;
+
+export const DeleteButton = styled.button`
+  width: 3rem;
+  height: 3rem;
+
+  &:hover {
+    background-color: rgb(255, 211, 117, 0.1);
+  }
+`;
+
+export const AddWrapper = styled.div`
+  width: 100%;
+
+  display: flex;
+  justify-content: center;
+`;
+
+export const Add = styled.div`
+  width: 3rem;
+  height: 3rem;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  border: 1px solid rgb(220, 220, 220);
+  border-radius: 50%;
+  padding-bottom: 0.5rem;
+
+  font-size: 2rem;
+
+  color: rgb(220, 220, 220);
+
+  cursor: pointer;
+
+  &:hover {
+    color: black;
+    border: 1px solid black;
+  }
 `;
 
 /* Setting */
 export const SettingWrapper = styled.div`
-  height: 10%;
+  height: 5rem;
 
   display: flex;
   justify-content: center;
