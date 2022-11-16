0a4d85d81ae15796e62eec5c45367ca82d74f439
YouYou94 <ybh4115@gmail.com>
2022-11-16 15:47:42 +0900

    Add. 불렛 아이콘 옵션 추가

--- src/components/Bullet/BulletOption.jsx
+++ src/components/Bullet/BulletOption.jsx
@@ -1,7 +1,50 @@
 import * as Styled from './BulletStyled.jsx';
+import square from '../../img/BulletIcon/square.png';
+import fillsquare from '../../img/BulletIcon/fillsquare.png';
+import triangle from '../../img/BulletIcon/triangle.png';
+import filltriangle from '../../img/BulletIcon/filltriangle.png';
+import circle from '../../img/BulletIcon/circle.png';
+import fillcircle from '../../img/BulletIcon/fillcircle.png';
+import dot from '../../img/BulletIcon/dot.png';
+import x from '../../img/BulletIcon/x.png';
+import up from '../../img/BulletIcon/up.png';
+import down from '../../img/BulletIcon/down.png';
+import next from '../../img/BulletIcon/next.png';
+import prev from '../../img/BulletIcon/prev.png';
+import cake from '../../img/BulletIcon/cake.png';
+import star from '../../img/BulletIcon/star.png';
+import heart from '../../img/BulletIcon/heart.png';
+import music from '../../img/BulletIcon/music.png';
+
+const itemList = [
+  square,
+  fillsquare,
+  triangle,
+  filltriangle,
+  circle,
+  fillcircle,
+  dot,
+  x,
+  up,
+  down,
+  next,
+  prev,
+  cake,
+  star,
+  heart,
+  music,
+];
 
 const BulletOption = () => {
-  return <Styled.OptionWrapper></Styled.OptionWrapper>;
+  const option = itemList.map((item, index) => {
+    return (
+      <Styled.Option key={index}>
+        <Styled.OptionItem icon={item} />
+      </Styled.Option>
+    );
+  });
+
+  return <Styled.OptionWrapper>{option}</Styled.OptionWrapper>;
 };
 
 export default BulletOption;
--- src/components/Bullet/BulletStyled.jsx
+++ src/components/Bullet/BulletStyled.jsx
@@ -14,7 +14,42 @@ export const Title = styled.h1`
 `;
 
 export const OptionWrapper = styled.div`
-  height: 85%;
+  flex: 1;
+
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  align-content: flex-start;
+
+  padding: 2rem;
 `;
 
-export const Option = styled.div``;
+export const Option = styled.div`
+  min-width: 4rem;
+  min-height: 4rem;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  margin: 1rem;
+  border: 1px solid rgb(220, 220, 220);
+  border-radius: 1.5rem;
+
+  background-color: white;
+
+  cursor: pointer;
+
+  &:hover {
+    background-color: rgb(255, 211, 117, 0.2);
+  }
+`;
+
+export const OptionItem = styled.div`
+  width: 50%;
+  height: 50%;
+
+  background-image: url(${prop => prop.icon});
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
