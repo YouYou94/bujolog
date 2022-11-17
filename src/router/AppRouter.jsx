afda09f4ff57c80053406722ac0388aa7671e607
YouYou94 <ybh4115@gmail.com>
2022-11-17 09:12:50 +0900

    :fire: 디자인 변경 시작!

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,27 +1,7 @@
 import { Route, Routes, useLocation } from 'react-router-dom';
-import BulletContainer from '../containers/Journal/BulletContainer';
-import TopBar from '../containers/TopBar/TopBarContainer';
-import AuthPage from '../pages/AuthPage';
-import MyPage from '../pages/MyPage';
-import NotFound from '../pages/NotFound';
-
-const WRITEURL = ['/bujolog/my/bullet', '/bujolog/my/journal'];
 
 const AppRouter = () => {
-  const location = useLocation().pathname;
-
-  return (
-    <>
-      {WRITEURL.find(path => path === location) ? <></> : <TopBar />}
-      <Routes>
-        <Route path="*" element={<NotFound />} />
-        <Route path="/bujolog/auth/login" element={<AuthPage />} />
-        <Route path="/bujolog/my" element={<MyPage />}>
-          <Route path="bullet" element={<BulletContainer />} />
-        </Route>
-      </Routes>
-    </>
-  );
+  return <></>;
 };
 
 export default AppRouter;
