8cf458798d2e58f6dad9a498c531b44d0e99c954
YouYou94 <ybh4115@gmail.com>
2022-11-17 12:22:24 +0900

    :fire:

--- src/components/Auth/AuthButton.jsx
+++
@@ -1,7 +0,0 @@
-import * as Styled from './AuthStyled.jsx';
-
-const AuthButton = ({ id, pw }) => {
-  return <Styled.Button>로그인</Styled.Button>;
-};
-
-export default AuthButton;
--- src/components/Bullet/BulletTitle.jsx
+++
@@ -1,7 +0,0 @@
-import * as Styled from './BulletStyled.jsx';
-
-const BulletTitle = () => {
-  return <Styled.Title>Bullet Key</Styled.Title>;
-};
-
-export default BulletTitle;
