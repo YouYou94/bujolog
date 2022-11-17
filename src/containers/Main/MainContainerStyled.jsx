a57665d88c2d29ce8578aacd48a704db54f46809
YouYou94 <ybh4115@gmail.com>
2022-11-17 12:23:12 +0900

    :art: MainContainer 반응형 웹 코드 추가

--- src/containers/Main/MainContainerStyled.jsx
+++ src/containers/Main/MainContainerStyled.jsx
@@ -5,5 +5,21 @@ export const Container = styled.div`
 
   display: flex;
 
+  padding: 3rem 10rem;
+
   background-color: rgb(245, 246, 247, 0.2);
+
+  @media screen and (max-width: 1440px) {
+    padding: 3rem 3rem;
+  }
+
+  @media screen and (max-width: 1024px) {
+    flex-direction: column;
+
+    padding: 3rem 10rem;
+  }
+
+  @media screen and (max-width: 768px) {
+    padding: 3rem 3rem;
+  }
 `;
