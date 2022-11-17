d503798a6b9512b91b115feb3e3ccb73c947b7a2
YouYou94 <ybh4115@gmail.com>
2022-11-17 14:50:36 +0900

    :hammer: 코드 수정

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,8 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 
 /* Containers */
-import TopBar from '../containers/TopBar/TopBar.jsx';
-import Footer from '../containers/Footer/Footer.jsx';
 import Main from '../containers/Main/Main.jsx';
 
 /* Pages */
@@ -12,14 +10,12 @@ import AuthPage from '../pages/AuthPage.jsx';
 const AppRouter = () => {
   return (
     <>
-      <TopBar />
       <Routes>
         <Route path="/bujolog" element={<Main />}>
           <Route path="" element={<CoverPage />} />
           <Route path="auth/login" element={<AuthPage />} />
         </Route>
       </Routes>
-      <Footer />
     </>
   );
 };
