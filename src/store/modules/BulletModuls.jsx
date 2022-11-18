0ba9bcf603edc0ddc7492e15a89d171208d74f4a
YouYou94 <ybh4115@gmail.com>
2022-11-18 18:11:01 +0900

    :hammer: Redux Bullet Moduls CRUD 수정 및 스테이트 추가, 변경

--- src/store/modules/BulletModuls.jsx
+++ src/store/modules/BulletModuls.jsx
@@ -1,45 +1,68 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { DOT, SQUARE, NEXT, PREV } from '../../Constants.jsx';
 
-const initialState = {
+const commonState = {
   bullet: [
     {
       key: DOT,
       description: '해야 할 일',
+      save: true,
     },
     {
       key: SQUARE,
       description: '완료',
+      save: true,
     },
     {
       key: NEXT,
       description: '일정 연기',
+      save: true,
     },
     {
       key: PREV,
       description: '일정 변경',
+      save: true,
     },
   ],
 };
 
+const userState = {
+  is: 'NEWBIE',
+  bullet: commonState.bullet,
+};
+
 const bulletSlice = createSlice({
   name: 'bullet',
-  initialState,
+  initialState: { commonState, userState },
   reducers: {
     addBullet(state, action) {
-      state.bullet.push(action.payload);
+      state.commonState.bullet.push(action.payload);
     },
     delBullet(state, action) {
-      state.bullet = state.bullet.filter(
-        (e, index) => index !== action.payload
+      const id = action.payload;
+      state.commonState.bullet = state.commonState.bullet.filter(
+        (e, index) => index !== id
       );
     },
     saveBullet(state, action) {
       const { id, key, description } = action.payload;
-      state.bullet[id] = { key: key, description: description };
+      state.commonState.bullet[id] = {
+        key: key,
+        description: description,
+        save: true,
+      };
+    },
+    saveAllBullet(state) {
+      state.userState.is = 'OLDBIE';
+      state.userState.bullet = state.commonState.bullet.filter(
+        bullet => bullet.save === true
+      );
+
+      state.commonState.bullet = state.userState.bullet;
     },
   },
 });
 
-export const { addBullet, delBullet, saveBullet } = bulletSlice.actions;
+export const { addBullet, delBullet, saveBullet, saveAllBullet } =
+  bulletSlice.actions;
 export default bulletSlice;
