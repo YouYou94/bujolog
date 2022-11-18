63b4c0eec17a696577b4ecf775808daceb6fa715
YouYou94 <ybh4115@gmail.com>
2022-11-18 10:59:37 +0900

    :hammer: Redux 불렛 모듈 코드 수정

--- src/store/modules/BulletModuls.jsx
+++ src/store/modules/BulletModuls.jsx
@@ -1,51 +1,42 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { DOT, SQUARE, NEXT, PREV } from '../../Constants.jsx';
 
-const commonState = {
+const initialState = {
   bullet: [
     {
       key: DOT,
-      name: 'Task',
       description: '해야 할 일',
     },
     {
       key: SQUARE,
-      name: 'Task Completed',
       description: '완료',
     },
     {
       key: NEXT,
-      name: 'Task Migrated',
       description: '일정 연기',
     },
     {
       key: PREV,
-      name: 'Task Scheduled',
       description: '일정 변경',
     },
   ],
 };
 
-const userState = {
-  user: 'NEWBIE',
-  bullet: [],
-};
-
 const bulletSlice = createSlice({
   name: 'bullet',
-  initialState: { commonState, userState },
+  initialState,
   reducers: {
     addBullet(state, action) {
-      state.commonState.bullet.push(action.payload);
+      state.bullet.push(action.payload);
     },
     delBullet(state, action) {
-      state.commonState.bullet = state.commonState.bullet.filter(
+      state.bullet = state.bullet.filter(
         (e, index) => index !== action.payload
       );
     },
-    saveBullet(state) {
-      state.userState.user = 'OLDBIE';
-      state.userState.bullet = state.commonState.bullet;
+    saveBullet(state, action) {
+      const { id, key, description } = action.payload;
+      state.bullet[id] = { key: key, description: description };
     },
   },
 });
