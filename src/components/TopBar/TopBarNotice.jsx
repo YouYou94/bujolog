ccc36eef74de39c865980905bd29b59fe0f7ed22
YouYou94 <ybh4115@gmail.com>
2022-11-18 10:58:00 +0900

    :hammer: 코드 정리

--- src/components/TopBar/TopBarNotice.jsx
+++
@@ -1,7 +0,0 @@
-import * as Styled from './TopBarComponentStyled.jsx';
-
-const TopBarNotice = () => {
-  return <Styled.NoticeWrapper></Styled.NoticeWrapper>;
-};
-
-export default TopBarNotice;
--- src/containers/Footer/Footer.jsx
+++
@@ -1,7 +0,0 @@
-import * as Styled from './FooterContainerStyled.jsx';
-
-const Footer = () => {
-  return <Styled.Container></Styled.Container>;
-};
-
-export default Footer;
