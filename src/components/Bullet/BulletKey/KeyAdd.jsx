58cf72874a8994018c610ea5690248cbc1d8d018
YouYou94 <ybh4115@gmail.com>
2022-11-18 18:16:08 +0900

    :hammer: 코드 추가 및 수정

--- src/components/Bullet/BulletKey/KeyAdd.jsx
+++ src/components/Bullet/BulletKey/KeyAdd.jsx
@@ -1,11 +1,16 @@
+import { useDispatch } from 'react-redux';
+import { addBullet } from '../../../store/modules/BulletModuls.jsx';
 import * as Styled from './BulletKeyStyled.jsx';
 
 const KeyAdd = () => {
-  const modeIsAdd = () => {};
+  const dispatch = useDispatch();
+
+  const CreateDefaultBullet = () =>
+    dispatch(addBullet({ key: null, description: '', save: false }));
 
   return (
     <Styled.AddWrapper>
-      <Styled.AddButton onClick={modeIsAdd}>Add</Styled.AddButton>
+      <Styled.AddButton onClick={CreateDefaultBullet}>Add</Styled.AddButton>
     </Styled.AddWrapper>
   );
 };
--- src/pages/BulletPage.jsx
+++ src/pages/BulletPage.jsx
@@ -1,16 +1,24 @@
+import { createContext, useState } from 'react';
 import BulletCaption from '../components/Bullet/BulletCaption.jsx';
 import BulletContents from '../components/Bullet/BulletContents.jsx';
 import * as Styled from './PageStyled.jsx';
 
+export const BulletContext = createContext();
+
 const BulletPage = () => {
+  const [isTool, setIsTool] = useState(false);
+  const [key, setKey] = useState('');
+
   return (
     <>
-      <Styled.Caption>
-        <BulletCaption />
-      </Styled.Caption>
-      <Styled.Contents>
-        <BulletContents />
-      </Styled.Contents>
+      <BulletContext.Provider value={{ isTool, setIsTool, key, setKey }}>
+        <Styled.Caption>
+          <BulletCaption />
+        </Styled.Caption>
+        <Styled.Contents>
+          <BulletContents />
+        </Styled.Contents>
+      </BulletContext.Provider>
     </>
   );
 };
