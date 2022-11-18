6397100e2e1b3afe9abc740049eb7ab963db27df
YouYou94 <ybh4115@gmail.com>
2022-11-19 00:51:10 +0900

    :hammer: Icon 나열 및 아이콘 선택 기능 코드 추가

--- src/components/Bullet/BulletTool/ToolArticle.jsx
+++ src/components/Bullet/BulletTool/ToolArticle.jsx
@@ -1,11 +1,19 @@
+import { useContext } from 'react';
 import { ICONLIST } from '../../../Constants.jsx';
+import { BulletContext } from '../../../pages/BulletPage.jsx';
 import * as Styled from './ToolStyled.jsx';
 
 const ToolArticle = () => {
+  const { setIsTool, setKeyIcon } = useContext(BulletContext);
+  const selectIcon = event => {
+    setIsTool(false);
+    setKeyIcon(event.target.parentElement.id);
+  };
+
   const option = ICONLIST.map((item, index) => {
     return (
-      <Styled.OptionWrapper key={index}>
-        <Styled.Option icon={item} />
+      <Styled.OptionWrapper key={index} id={item}>
+        <Styled.Option icon={item} onClick={selectIcon} />
       </Styled.OptionWrapper>
     );
   });
