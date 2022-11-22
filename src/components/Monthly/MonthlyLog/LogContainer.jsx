13f7a98cfb59c61e33ef772516376ad4537e1b03
YouYou94 <ybh4115@gmail.com>
2022-11-22 23:23:58 +0900

    :hammer: Context value 추가

--- src/components/Monthly/MonthlyLog/LogContainer.jsx
+++ src/components/Monthly/MonthlyLog/LogContainer.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 import LogTitle from './Header/LogTitle.jsx';
 
@@ -9,30 +9,44 @@ import Goals from './Section/Goals.jsx';
 import MonthlyBack from './Footer/MonthlyBack.jsx';
 import MonthlyConfirm from './Footer/MonthlyConfirm.jsx';
 
+import MonthlyTool from './Tool/MonthlyTool.jsx';
+
+import { createContext } from 'react';
+
 import * as Styled from './LogStyled.jsx';
 
 export const TASK = 'Task';
 export const GOAL = 'goal';
+export const LogContext = createContext();
 
 const MonthlyRightContainer = () => {
+  const toolRef = useRef();
+  const logRef = useRef();
+  const [isTool, setIsTool] = useState(false);
+  const [keyIcon, setKeyIcon] = useState('');
   const [tap, setTap] = useState(TASK);
 
-  const whatIsLog = tap === TASK ? <Tasks /> : <Goals />;
+  const whatIsLog = tap === TASK ? <Tasks tap={tap} /> : <Goals tap={tap} />;
+
+  const value = { toolRef, logRef, isTool, setIsTool, keyIcon, setKeyIcon };
 
   return (
-    <Styled.RightContainer>
-      <Styled.RightHeader>
-        <LogTitle />
-      </Styled.RightHeader>
-      <Styled.RightSection>
-        <TabMenu tap={tap} setTap={setTap} />
-        {whatIsLog}
-      </Styled.RightSection>
-      <Styled.RightFooter>
-        <MonthlyBack />
-        <MonthlyConfirm />
-      </Styled.RightFooter>
-    </Styled.RightContainer>
+    <LogContext.Provider value={value}>
+      <Styled.RightContainer>
+        <MonthlyTool />
+        <Styled.RightHeader>
+          <LogTitle />
+        </Styled.RightHeader>
+        <Styled.RightSection>
+          <TabMenu tap={tap} setTap={setTap} />
+          {whatIsLog}
+        </Styled.RightSection>
+        <Styled.RightFooter>
+          <MonthlyBack />
+          <MonthlyConfirm />
+        </Styled.RightFooter>
+      </Styled.RightContainer>
+    </LogContext.Provider>
   );
 };
 
