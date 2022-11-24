0c47e8b00a6feba6402d9fb4e6d71fd1210d24f4
YouYou94 <ybh4115@gmail.com>
2022-11-24 12:02:22 +0900

    :hammer: Write Context 생성 및 Provider

--- src/components/Daily/Write/WriteContainer.jsx
+++ src/components/Daily/Write/WriteContainer.jsx
@@ -1,3 +1,6 @@
+import { useRef } from 'react';
+import { createContext } from 'react';
+import { useState } from 'react';
 import WriteBack from './Footer/WriteBack.jsx';
 import WriteConfirm from './Footer/WriteConfirm.jsx';
 import WriteTitle from './Header/WriteTitle.jsx';
@@ -5,21 +8,31 @@ import DailyLog from './Section/DailyLog.jsx';
 import * as Styled from './WriteStyled.jsx';
 
 export const Today = new Date();
+export const DailyContext = createContext();
 
 const WriteContainer = () => {
+  const addRef = useRef();
+  const toolRef = useRef();
+  const [isTool, setIsTool] = useState(false);
+  const [keyIcon, setKeyIcon] = useState('');
+
+  const value = { addRef, toolRef, isTool, setIsTool, keyIcon, setKeyIcon };
+
   return (
-    <Styled.Container>
-      <Styled.WriteHeader>
-        <WriteTitle />
-      </Styled.WriteHeader>
-      <Styled.WriteSection>
-        <DailyLog />
-      </Styled.WriteSection>
-      <Styled.WriteFooter>
-        <WriteBack />
-        <WriteConfirm />
-      </Styled.WriteFooter>
-    </Styled.Container>
+    <DailyContext.Provider value={value}>
+      <Styled.Container>
+        <Styled.WriteHeader>
+          <WriteTitle />
+        </Styled.WriteHeader>
+        <Styled.WriteSection>
+          <DailyLog />
+        </Styled.WriteSection>
+        <Styled.WriteFooter>
+          <WriteBack />
+          <WriteConfirm />
+        </Styled.WriteFooter>
+      </Styled.Container>
+    </DailyContext.Provider>
   );
 };
 
