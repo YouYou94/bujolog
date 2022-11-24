950fa5d33438aa555c486533d005a3381d2b90bf
YouYou94 <ybh4115@gmail.com>
2022-11-24 15:30:31 +0900

    :hammer: 데일리 로그 추가 기능 구현

--- src/components/Daily/Write/Section/DailyLog.jsx
+++ src/components/Daily/Write/Section/DailyLog.jsx
@@ -1,24 +1,40 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
-import { useContext } from 'react';
-import { useEffect } from 'react';
-import { useState } from 'react';
-import { DailyContext } from '../WriteContainer.jsx';
+import { useContext, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { nanoid } from '@reduxjs/toolkit';
+import { DailyContext, Today } from '../WriteContainer.jsx';
 import * as Styled from './WriteSectionStyled.jsx';
+import {
+  addDailyLog,
+  checkTodayLog,
+} from '../../../../store/modules/DailyModule.jsx';
 
 const DailyLog = () => {
-  const { addRef, toolRef, isTool, setIsTool, keyIcon } =
+  const dispatch = useDispatch();
+  const { addRef, toolRef, isTool, setIsTool, keyIcon, setKeyIcon } =
     useContext(DailyContext);
   const [isAdd, setIsAdd] = useState(false);
   const [dailyLog, setDailyLog] = useState('');
+  const today = `${Today.getMonth() + 1}${Today.getDate()}`;
+  const dailyState = useSelector(state => state.daily.daily);
 
+  // 기능
   const usingTool = () => setIsTool(!isTool);
 
   const handleOnChangeLog = event => setDailyLog(event.target.value);
 
   const handleOnClickAddDailyLog = () => {
-    console.log(dailyLog);
+    if (keyIcon && dailyLog) {
+      if (!dailyState[`${today}`]) dispatch(checkTodayLog({ today: today }));
+
+      dispatch(addDailyLog({ today: today, icon: keyIcon, log: dailyLog }));
+
+      setIsAdd(false);
+    }
   };
+
+  const handleOnClickDelDailyLog = () => {};
 
   const handleOnKeyPressDailyLog = event => {
     if (event.key === 'Enter') handleOnClickAddDailyLog();
@@ -26,7 +42,11 @@ const DailyLog = () => {
 
   // 해당 영역 밖으로 나가면 Tool 메뉴 안 보이게
   const handleClickOutSide = event => {
-    if (isAdd && !addRef.current.contains(event.target)) {
+    if (
+      isAdd &&
+      !addRef.current.contains(event.target) &&
+      !toolRef.current.contains(event.target)
+    ) {
       setIsAdd(false);
     }
   };
@@ -38,34 +58,64 @@ const DailyLog = () => {
     };
   });
 
+  const initialSetState = () => {
+    setKeyIcon('');
+    setDailyLog('');
+    setIsTool(false);
+  };
+
+  useEffect(() => {
+    if (!isAdd) initialSetState();
+  });
+
+  const dailyLogList = dailyState[`${today}`]
+    ? dailyState[`${today}`].map(item => {
+        const { icon, log } = item;
+
+        return (
+          <Styled.DailyBox key={nanoid()}>
+            <Styled.Key icon={icon} />
+            <Styled.Log>{log}</Styled.Log>
+            <Styled.ButtonWrapper>
+              <Styled.SettingButton
+                onClick={handleOnClickDelDailyLog}
+                log={true}
+              >
+                삭제하기
+              </Styled.SettingButton>
+            </Styled.ButtonWrapper>
+          </Styled.DailyBox>
+        );
+      })
+    : '';
+
   return (
     <Styled.DailyLogLayout>
       <Styled.LogBox>
-        <Styled.AddBox ref={addRef}>
+        {dailyLogList}
+        <Styled.DailyBox ref={addRef}>
           {isAdd ? (
-            <Styled.AddItem>
-              <Styled.KeyWrapper onClick={usingTool}>
-                <Styled.Key icon={keyIcon}>
-                  {keyIcon ? '' : <FontAwesomeIcon icon={faPlus} />}
-                </Styled.Key>
-              </Styled.KeyWrapper>
+            <>
+              <Styled.AddKey icon={keyIcon} onClick={usingTool}>
+                {keyIcon ? '' : <FontAwesomeIcon icon={faPlus} />}
+              </Styled.AddKey>
               <Styled.LogInput
                 value={dailyLog}
                 onChange={handleOnChangeLog}
                 onKeyPress={handleOnKeyPressDailyLog}
               />
               <Styled.ButtonWrapper>
-                <Styled.AddButton onClick={handleOnClickAddDailyLog}>
+                <Styled.SettingButton onClick={handleOnClickAddDailyLog}>
                   추가하기
-                </Styled.AddButton>
+                </Styled.SettingButton>
               </Styled.ButtonWrapper>
-            </Styled.AddItem>
+            </>
           ) : (
             <Styled.AddButton onClick={() => setIsAdd(!isAdd)}>
               클릭하여 추가하기
             </Styled.AddButton>
           )}
-        </Styled.AddBox>
+        </Styled.DailyBox>
       </Styled.LogBox>
     </Styled.DailyLogLayout>
   );
