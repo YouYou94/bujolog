e75912fda2ffd40993ee0fd732246975068ba7e4
YouYou94 <ybh4115@gmail.com>
2023-01-05 17:55:09 +0900

    Add: 공통 컴포넌트 Container 생성

--- src/components/common/Container.jsx
+++ src/components/common/Container.jsx
@@ -1,11 +1,45 @@
 import styled from 'styled-components';
 
 export const CommonContainer = ({ children }) => {
-  return;
+  return <MainLayout>{children}</MainLayout>;
+};
+
+export const WriteContainer = ({ children }) => {
+  return <WriteLayout>{children}</WriteLayout>;
+};
+
+export const ViewContainer = ({ children }) => {
+  return <ViewLayout>{children}</ViewLayout>;
 };
 
 /* 추후 반응형 웹 만들기! */
-export const Container = styled.div`
+export const MainLayout = styled.div`
   width: 100vw;
   height: 100vh;
+
+  display: flex;
+  flex-direction: row;
+`;
+
+/* 작성 컨테이너 */
+export const WriteLayout = styled.div`
+  flex: 1;
+
+  display: flex;
+  flex-direction: column;
+
+  border-right: 2px solid rgb(204, 204, 204);
+  padding: 2rem;
+`;
+
+/* 결과 컨테이너 */
+export const ViewLayout = styled.div`
+  flex: 1;
+
+  display: flex;
+  flex-direction: column;
+
+  padding: 2rem;
+
+  background-color: rgb(245, 245, 245);
 `;
---
+++ src/components/index.jsx
@@ -0,0 +1,5 @@
+export {
+  CommonContainer,
+  WriteContainer,
+  ViewContainer,
+} from './common/Container.jsx';
