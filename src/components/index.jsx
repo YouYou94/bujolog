ca03d06a8b711901dc2d8178ce3a0fdee0300ae4
YouYou94 <ybh4115@gmail.com>
2023-01-11 13:58:50 +0900

    Add: 공통의 UI 이미지가 있는 레이아웃 생성

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,20 +1,5 @@
 /* Common UI */
-/* Layout */
-export { BackgroundLayout, DefaultLayout } from './common/Layout/CommonLayout';
-export {
-  SetupTemplate,
-  SetupArticle,
-  SetupFooter,
-} from './common/Layout/SetupLayout';
-
-/* Button */
-export { SettingButton } from './common/Button/Button';
+export { BackgroundLayout } from './common/Layout/BackgroundLayout';
 
 /* Components */
 /* Home */
-export { EnrolUser } from './Home/EnrolUser';
-
-/* Menual */
-export { KeySettingMenual } from './Menual/KeySettingMenual';
-export { MonthlyMenual } from './Menual/MonthlyMenual';
-export { DailyMenual } from './Menual/DailyMenual';
