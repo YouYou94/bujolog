788015d5e63d697b5f352d6d058b11f9b2c689c8
YouYou94 <ybh4115@gmail.com>
2023-01-12 20:15:01 +0900

    컴포넌트 export

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,5 +1,12 @@
 /* Common UI */
-export { BackgroundLayout } from './common/Layout/BackgroundLayout';
+export { ImageLayout, DefaultLayout } from './common/Layout/Layout';
+export { Title } from './common/Title/Title';
+export { Menual } from './common/Menual/Menual';
+export { SettingButton } from './common/Button/Button';
+export { ButtonBox } from './common/Box/Box';
 
 /* Components */
-/* Home */
+export { default as HomeContainer } from './Home/HomeContainer';
+export { default as IndexContainer } from './Index/IndexContainer';
+export { default as KeySettingContainer } from './KeySetting/keySettingContainer';
+export { default as MonthlyContainer } from './Monthly/MonthlyContainer';
