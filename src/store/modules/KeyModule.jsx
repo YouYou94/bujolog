32f186863b3b54d0ecfa5197042d2f865f1daf38
YouYou94 <ybh4115@gmail.com>
2023-01-12 13:30:05 +0900

    Feat: Redux KeyModule CRUD 기능 구현

--- src/store/modules/KeyModule.jsx
+++ src/store/modules/KeyModule.jsx
@@ -1,16 +1,77 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { INITIALKEY } from '../../Constants';
 
-const initialState = JSON.parse(localStorage.getItem('user')).key;
+const initialState = {
+  key: localStorage.getItem('user')
+    ? JSON.parse(localStorage.getItem('user')).key
+    : INITIALKEY,
+};
 
 const keySlice = createSlice({
   name: 'key',
   initialState,
   reducers: {
-    createKey(state, action) {},
-    updateKey(state, action) {},
-    deleteKey(state, action) {},
-    initialBullet(state) {
-      state.bullet = state.bullet.filter(bullet => bullet.save === true);
+    createKey(state, action) {
+      const { selectedKey } = action.payload;
+      const { icon, id, description } = selectedKey;
+
+      const createState = {
+        icon,
+        id,
+        description,
+      };
+
+      const userState = JSON.parse(localStorage.getItem('user'));
+
+      state.key.push(createState);
+
+      localStorage.setItem(
+        'user',
+        JSON.stringify({ ...userState, key: state.key })
+      );
+
+      console.log('키 생성 완료!');
+    },
+    updateKey(state, action) {
+      const { selectedKey } = action.payload;
+      const { icon, id, description } = selectedKey;
+
+      const updateState = {
+        icon,
+        id,
+        description,
+      };
+
+      const userState = JSON.parse(localStorage.getItem('user'));
+
+      const changeState = state.key.map(key =>
+        key.id === id ? updateState : key
+      );
+
+      state.key = changeState;
+
+      localStorage.setItem(
+        'user',
+        JSON.stringify({ ...userState, key: state.key })
+      );
+
+      console.log('키 변경 완료!');
+    },
+    deleteKey(state, action) {
+      const { id } = action.payload;
+
+      const userState = JSON.parse(localStorage.getItem('user'));
+
+      const changeState = state.key.filter(key => key.id !== id);
+
+      state.key = changeState;
+
+      localStorage.setItem(
+        'user',
+        JSON.stringify({ ...userState, key: state.key })
+      );
+
+      console.log('키 삭제 완료!');
     },
   },
 });
