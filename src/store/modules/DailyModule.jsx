e89c325c79199c0ebd4ac891d87bb592a3f13d69
YouYou94 <ybh4115@gmail.com>
2023-01-17 16:56:23 +0900

    Add: 초기 스테이트 로컬스토리지에서 가져오기 / create 데일리로그 구현

--- src/store/modules/DailyModule.jsx
+++ src/store/modules/DailyModule.jsx
@@ -1,36 +1,48 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  daily: {},
+  daily: localStorage.getItem('user')
+    ? JSON.parse(localStorage.getItem('user')).dailylog
+    : {},
 };
 
 const daillySlice = createSlice({
   name: 'daily',
   initialState,
   reducers: {
-    checkTodayLog(state, action) {
-      const { today } = action.payload;
+    createDailyLog(state, action) {
+      const { createState } = action.payload;
+      const { id, today, key, log } = createState;
 
-      state.daily = {
-        ...state.daily,
-        [`${today}`]: [],
-      };
-    },
-    addDailyLog(state, action) {
-      const { today, icon, log } = action.payload;
+      if (!state.daily[`${today}`]) {
+        state.daily = {
+          ...state.daily,
+          [`${today}`]: [],
+        };
+      }
 
-      if (!state.daily[`${today}`]) checkTodayLog({ today: today });
+      if (state.daily[`${today}`]) {
+        state.daily[`${today}`].push({ id, key, log });
+      }
 
-      state.daily[`${today}`].push({ icon: icon, log: log });
-    },
-    delDailyLog(state, action) {
-      const { today, id } = action.payload;
-      state.daily[`${today}`] = state.daily[`${today}`].filter(
-        (e, index) => index !== Number(id)
+      const userState = JSON.parse(localStorage.getItem('user'));
+
+      localStorage.setItem(
+        'user',
+        JSON.stringify({
+          ...userState,
+          dailylog: {
+            ...userState.dailylog,
+            [`${today}`]: state.daily[`${today}`],
+          },
+        })
       );
+
+      console.log('데일리로그 추가 완료!');
     },
+    deleteDailyLog(state, action) {},
   },
 });
 
-export const { checkTodayLog, addDailyLog, delDailyLog } = daillySlice.actions;
+export const { createDailyLog, deleteDailyLog } = daillySlice.actions;
 export default daillySlice;
